86b639c481e5b47d80c843e332f4405b02a20e7b
flowz <wolfzcel@gmail.com>
2025-01-29 12:33:47 -0400

    links -zoom

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -77,29 +77,36 @@ export const UI = ({ hidden, ...props }) => {
       </div>
 
       <div className="absolute top-4 right-4 flex gap-4">
+      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
         <button className="pointer-events-auto bg-gradient-to-r from-pink-500 to-orange-500 hover:from-pink-600 hover:to-orange-600 text-white p-2 rounded-full">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
           <path d="M7.75 2h8.5C19.55 2 22 4.45 22 7.75v8.5C22 19.55 19.55 22 16.25 22h-8.5C4.45 22 2 19.55 2 16.25v-8.5C2 4.45 4.45 2 7.75 2zm0-2C3.47 0 0 3.47 0 7.75v8.5C0 20.53 3.47 24 7.75 24h8.5C20.53 24 24 20.53 24 16.25v-8.5C24 3.47 20.53 0 16.25 0h-8.5z"/>
           <path d="M12 6.25a5.75 5.75 0 100 11.5 5.75 5.75 0 000-11.5zm0 9.5a3.75 3.75 0 110-7.5 3.75 3.75 0 010 7.5zM18.5 5.75a1.25 1.25 0 100 2.5 1.25 1.25 0 000-2.5z"/>
         </svg>
         </button>
+      </a>
 
+      <a href="https://x.com" target="_blank" rel="noopener noreferrer">
         <button className="pointer-events-auto bg-black hover:bg-gray-800 text-white p-2 rounded-full">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
           <path d="M18.36 2H21l-7.56 8.63L22 22h-6.86l-5.12-6.9L5.09 22H2.36l8.06-9.2L2 2h6.86l4.66 6.29L18.36 2zm-1.21 17.95h2.05L8.18 4.05H6.13l11.02 15.9z"/>
           </svg>
         </button>
+      </a>
 
+      <a href="https://dexscreener.com" target="_blank" rel="noopener noreferrer">
         <button className="pointer-events-auto bg-black hover:bg-gray-800 text-white p-2 rounded-full flex items-center justify-center">
-           <img src="https://i.imgur.com/gPJacwG.png" alt="Dexscreener Logo" className="w-6 h-6"/>
-           <span className="ml-2 text-sm font-semibold">Dexscreener</span>
+          <img src="https://i.imgur.com/gPJacwG.png" alt="Dexscreener Logo" className="w-6 h-6"/>
+          <span className="ml-2 text-sm font-semibold">Dexscreener</span>
         </button>
+      </a>
+
 
 
       </div>
 
       <div className="w-full flex flex-col items-end justify-center gap-4">
-        <button onClick={() => setCameraZoomed(!cameraZoomed)} className="pointer-events-auto bg-sky-500 hover:bg-sky-600 text-white p-4 rounded-md">
+        {/* <button onClick={() => setCameraZoomed(!cameraZoomed)} className="pointer-events-auto bg-sky-500 hover:bg-sky-600 text-white p-4 rounded-md">
           {cameraZoomed ? (
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM13.5 10.5h-6" />
@@ -109,7 +116,7 @@ export const UI = ({ hidden, ...props }) => {
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM10.5 7.5v6m3-3h-6" />
             </svg>
           )}
-        </button>
+        </button> */}
 
         <button onClick={toggleGreenScreen} className="pointer-events-auto bg-sky-500 hover:bg-sky-600 text-white p-4 rounded-md">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
